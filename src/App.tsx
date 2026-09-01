bb271fd8eda78b1dae929bb5966dc52523806aa4
bolognacarmine-cell <bologna.carmine@gmail.com>
2026-09-02 01:56:06 +0200

    fix: reset scroll position on route change from footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
+import ScrollToTop from "./components/ScrollToTop";
 import Home from "./pages/Home";
 import SectorPage from "./pages/SectorPage";
 import Projects from "./pages/Projects";
@@ -21,6 +22,7 @@ import AdminSettings from "./pages/admin/AdminSettings";
 export default function App() {
   return (
     <BrowserRouter>
+      <ScrollToTop />
       <Navbar />
       <Routes>
         {/* PUBLIC */}
